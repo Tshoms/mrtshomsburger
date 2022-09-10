1d536f90710d0f2eaf697a780b5921b0aad2d6a8
Tshoms <tshomsjordy@gmail.com>
2022-09-11 00:41:22 +0200

    feature/F02:add React Router v6 OrderPage and NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginForm from "./components/login/LoginForm";
+import OrderPage from "./components/pages/OrderPage";
 
 function App() {
   return (
     <div className="App">
-      <LoginForm />
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/Orderpage/:user" element={<OrderPage />} />
+      </Routes>
     </div>
   );
 }
