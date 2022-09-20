dd343be109a01bb2ee9766718b14cee1bd4ad0c8
Tshoms <tshomsjordy@gmail.com>
2022-09-21 00:49:47 +0200

    feature/F03: add React-icons and Google fonts

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -4,15 +4,39 @@ import LogoBrand from "../assets/LogoStyled.png";
 
 function Brand() {
   return (
-    <div>
-      <BrandLogo src={LogoBrand} alt="logo" />
-    </div>
+    <DivLogo>
+      <PLogo>Tshoms</PLogo>
+      <BrandLogo />
+      <PLogo>Burger</PLogo>
+    </DivLogo>
   );
 }
 
+const DivLogo = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 270px;
+  width: 55%;
+
+  justify-content: center;
+  align-items: center;
+`;
+
 const BrandLogo = styled.image`
-  height: 20px;
-  width: 20px;
+  height: 179px;
+  width: 40%;
+  background-image: url(${LogoBrand});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  marging: 10px;
+`;
+
+const PLogo = styled.p`
+  @import url("https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap");
+  font-family: "Amatic SC", cursive;
+  font-size: 55px;
+  color: orange;
 `;
 
 export default Brand;
--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -3,7 +3,8 @@ import { createSearchParams, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import burgerbackground from "../../assets/LoginPageStyled.jpg";
 import Brand from "../Brand";
-
+// import { FaUser } from "react-icons/fa";
+import { MdAccountCircle } from "react-icons/md";
 // import LogoStyled from "../LogoStyled";
 function LoginForm() {
   // state -----------------
@@ -34,14 +35,18 @@ function LoginForm() {
       <Brand />
       <LoginFormStyled onSubmit={handlesubmit}>
         <H1>Bienvenue chez nous !</H1>
-        <br />
+        <Liner />
         <H2>Connectez-vous</H2>
-        <Input
-          type="text"
-          placeholder="Entrez votre prénom..."
-          value={userName}
-          onChange={handlechange}
-        />
+        <InputStyled>
+          <MdAccountCircle size="20px" color="grey" />
+          <Input
+            type="text"
+            placeholder="Entrez votre prénom..."
+            value={userName}
+            onChange={handlechange}
+            required
+          />
+        </InputStyled>
 
         <Button>Accéder à mon espace</Button>
       </LoginFormStyled>
@@ -54,7 +59,6 @@ const LoginPageStyled = styled.div`
   flex-direction: column;
   background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),
     url(${burgerbackground});
-  // opacity: 0.33;
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
@@ -75,26 +79,52 @@ const LoginFormStyled = styled.form`
 `;
 
 const H1 = styled.h1`
+  @import url("https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap");
+  font-family: "Amatic SC", cursive;
   color: white;
   font-size: 35px;
 `;
 
 const H2 = styled.h1`
+  @import url("https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap");
+  font-family: "Amatic SC", cursive;
   color: white;
   font-size: 25px;
 `;
 
-const Input = styled.input`
-  height: 30px;
+const InputStyled = styled.div`
+  height: 40px;
   width: 50%;
+  background-color: white;
   margin-bottom: 20px;
+  border-radius: 5px;
+`;
+
+const Input = styled.input`
+  height: 20px;
+  width: 75%;
+  outline-width: 0;
+  margin: 15px;
+
+  border: none;
 `;
 
 const Button = styled.button`
-  height: 37px;
+  height: 40px;
   width: 51%;
   background-color: orange;
   color: white;
+  border-radius: 5px;
+  border: none;
+`;
+
+const Liner = styled.div`
+  border-top: 2px #ffca28 solid;
+  border-top-color: #ef6207;
+  border-top-style: solid;
+  border-top-width: 3px;
+  width: 60%;
+  margin-top: 10px;
 `;
 
 export default LoginForm;
