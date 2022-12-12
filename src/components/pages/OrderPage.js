23b02a0e4fed99036c0f0996fee0409eea544915
Tshoms <tshomsjordy@gmail.com>
2022-12-12 15:12:07 +0100

    feature/F04: > OrderPage Container Update

--- src/components/pages/OrderPage.js
+++ src/components/pages/OrderPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSearchParams } from "react-router-dom";
+import styled from "styled-components";
 import NavBar from "../NavBar";
 
 function OrderPage() {
@@ -10,11 +11,28 @@ function OrderPage() {
 
   // rendu ------------
   return (
-    <form>
-      <NavBar />
-      <h2>{`Bonjour ${userName}`}</h2>
-    </form>
+    <OrderStyled>
+      <div className="container">
+        <NavBar />
+        <h2>{`Bonjour ${userName}`}</h2>
+      </div>
+    </OrderStyled>
   );
 }
+
+const OrderStyled = styled.div`
+  background-color: orange;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .container {
+    background-color: red;
+    height: 95vh;
+    /* width: 1400px; */
+    width: 1200px;
+  }
+`;
 
 export default OrderPage;
