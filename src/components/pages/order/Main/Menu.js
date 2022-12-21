bd680957ebf32fcfdcc03d4983d1478fec65eea6
Tshoms <tshomsjordy@gmail.com>
2022-12-21 23:58:00 +0100

    feature/F05: OrderPage > Main > Menu > Add FakeMenu and Map method

--- src/components/pages/order/Main/Menu.js
+++ src/components/pages/order/Main/Menu.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
+import { fakeMenu1, fakeMenu2 } from "../../../../fakeData/fakeMenu";
 
 export default function Menu() {
-  return <MenuStyled>Menu</MenuStyled>;
+  /* state ----------- */
+  const [menu, setMenu] = useState(fakeMenu2);
+  return (
+    <MenuStyled>
+      {menu.map((produits) => {
+        return <div>{produits.title}</div>;
+      })}
+    </MenuStyled>
+  );
 }
 
 const MenuStyled = styled.div`
