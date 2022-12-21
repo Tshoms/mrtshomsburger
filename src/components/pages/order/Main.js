f3065e52aae6e4a63c0b4e4f77a7903dc876d68e
Tshoms <tshomsjordy@gmail.com>
2022-12-21 22:16:53 +0100

    feature/F05: OrderPage > Main > Add basket and menu w Css Grid

--- src/components/pages/order/Main.js
+++ src/components/pages/order/Main.js
@@ -3,7 +3,12 @@ import styled from "styled-components";
 import { theme } from "../../../theme";
 
 export default function Main() {
-  return <MainStyled></MainStyled>;
+  return (
+    <MainStyled>
+      <div className="basket">basket</div>
+      <div className="menu">menu</div>
+    </MainStyled>
+  );
 }
 
 const MainStyled = styled.div`
@@ -12,4 +17,15 @@ const MainStyled = styled.div`
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  /*css grid */
+  display: grid;
+  grid-template-columns: 25% 1fr; /* comportement en rows */
+
+  .basket {
+    background-color: pink;
+  }
+
+  .menu {
+    background-color: purple;
+  }
 `;
