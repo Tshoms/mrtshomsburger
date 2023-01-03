5781cdf8ea26e8f7a67093cd81c753416c814458
Tshoms <tshomsjordy@gmail.com>
2023-01-03 22:18:48 +0100

    feature/F06: NavbarRightSide > Add notification Toast in Admin

--- src/components/pages/order/Navbar/NavbarRightStyle.js
+++ src/components/pages/order/Navbar/NavbarRightStyle.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import ToggleButton from "../../../reusable-ui/ToggleButton";
 import Profile from "./Profile";
-// import { BsPersonCircle } from "react-icons/bs";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { theme } from "../../../../theme";
 
 export default function NavbarRightStyle({ userName }) {
+  const [isModeAdmin, setisModeAdmin] = useState(false);
+
+  const displayToastNotification = () => {
+    if (!isModeAdmin) {
+      toast.info("Mode admin activé", {
+        // icon: <FaUserSecret size={30} />,
+        theme: "dark",
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+    setisModeAdmin(!isModeAdmin);
+  };
+
   return (
     <NavbarRightSide>
       <ToggleButton
         labelIfUnchecked="ACTIVER LE MODE ADMIN"
         labelIfChecked="DÉSACTIVER LE MODE ADMIN"
+        onToggle={displayToastNotification}
       />
       <Profile userName={userName} />
+      <ToastContainer className="toaster" bodyClassName="body-toast" />
     </NavbarRightSide>
   );
 }
@@ -21,7 +44,21 @@ const NavbarRightSide = styled.div`
   align-items: center;
   padding-right: 50px;
 
-  /* .admin-container {
-    background: lightblue;
-  } */
+  .toaster {
+    max-width: 300px;
+  }
+
+  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+    background: ${theme.colors.background_dark};
+  }
+
+  .body-toast {
+    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+      margin-right: 20px;
+      margin-left: 5px;
+    }
+    div {
+      line-height: 1.3em;
+    }
+  }
 `;
