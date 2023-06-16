5e328a738feb8db9c2749753a1c3ca3676b8fd1c
Tshoms <tshomsjordy@gmail.com>
2023-06-16 22:11:53 +0200

    feature/F07: Profile.js > add useSearchParams .

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useSearchParams } from "react-router-dom";
+// import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
 import NavBar from "./Navbar/NavBar";
 import Main from "./Main/Main";
@@ -8,8 +8,8 @@ import OrderContext from "../../../context/OrderContext";
 
 function OrderPage() {
   // state ----------
-  const [searchParams] = useSearchParams();
-  const userName = searchParams.get("userName");
+  // const [searchParams] = useSearchParams();
+  // const userName = searchParams.get("userName");
   const [isModeAdmin, setIsModeAdmin] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [currentTabSelected, setCurrentTabSelected] = useState("add");
@@ -30,7 +30,7 @@ function OrderPage() {
     <OrderStyled>
       <div className="container">
         <OrderContext.Provider value={orderContextValue}>
-          <NavBar userName={userName} />
+          <NavBar />
           <Main />
         </OrderContext.Provider>
       </div>
