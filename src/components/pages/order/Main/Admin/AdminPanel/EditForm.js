ce4a8274fcb8cca709c921b8b4e23b0898cb91cd
Tshoms <tshomsjordy@gmail.com>
2023-06-28 15:33:36 +0200

    feature/F09: EditForm.js > import form from AddForm to EditForm ...

--- src/components/pages/order/Main/Admin/AdminPanel/EditForm.js
+++ src/components/pages/order/Main/Admin/AdminPanel/EditForm.js
@@ -1,28 +1,76 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import styled from "styled-components";
 import OrderContext from "../../../../../../context/OrderContext";
 import { theme } from "../../../../../../theme";
-import HintMessage from "./HintMessage";
-// import { HiCursorClick } from "react-icons/hi";
+import { EMPTY_PRODUCT } from "../../../../../enums/product";
+import TextInput from "../../../../../reusable-ui/TextInput";
+import ImagePreview from "./ImagePreview";
+import { getInputTextsConfig } from "./inputTextsConfig";
 
 export default function UpdateForm() {
   // state -----------
   const { productSelected } = useContext(OrderContext);
+  const [productBeingEdited, setProductBeingEdited] = useState(EMPTY_PRODUCT); // state inter à EditForm.
+
+  const inputTexts = getInputTextsConfig(productSelected);
+  // comportement ---------
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setProductBeingEdited({
+      ...productBeingEdited,
+      [name]: value,
+    });
+  };
   return (
     <TabUpdateStyled>
-      {/* <div className="container-update">
-        <span>cliquer sur un produit du menu pour le modifier</span>
-        <HiCursorClick className="icon" />
-      </div> */}
-      <HintMessage />
-      <span>{productSelected && productSelected.title}</span>
+      <ImagePreview
+        imageSource={productSelected.imageSource}
+        title={productSelected.title}
+      />
+      <div className="input-fields">
+        {inputTexts.map((input) => (
+          <TextInput
+            key={input.id}
+            {...input}
+            onChange={handleChange}
+            version="minimaliste"
+          />
+        ))}
+      </div>
     </TabUpdateStyled>
   );
 }
 
 const TabUpdateStyled = styled.div`
-  height: 278px;
-  width: 100%;
+  height: 100%;
+  width: 70%;
+
+  // grid.
+  display: grid;
+  grid-template-columns: 1fr 3fr; // nb colone (columns).
+  grid-template-rows: repeat(4, 1fr); // nb étage (rows).
+  grid-column-gap: 20px; // space betwen column w/ grid
+  grid-row-gap: 8px; // space betwen row w/ grid.
+
+  .input-fields {
+    grid-area: 1 / 2 / -2 / 2;
+
+    display: grid;
+    grid-row-gap: 8px;
+  }
+
+  .submit {
+    grid-area: 4 / -2 / -2 / -1;
+    display: flex;
+    align-items: center;
+
+    .submit-btn {
+      height: 100%;
+    }
+  }
+
+  //----- old ----------------
 
   /* .container-update {
     display: flex;
@@ -42,5 +90,6 @@ const TabUpdateStyled = styled.div`
       font-size: 29px;
       color: ${theme.colors.greyBlue};
     }
-  } */
+  } 
+  -----------------   */
 `;
