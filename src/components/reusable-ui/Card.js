3a66043b7acca1bd4cbab19beab55b4bee2e93cd
Tshoms <tshomsjordy@gmail.com>
2023-06-30 06:13:18 +0200

    feature/F09: Card.js > fix card height in hover effect...

--- src/components/reusable-ui/Card.js
+++ src/components/reusable-ui/Card.js
@@ -49,6 +49,7 @@ export default function Product({
 const CardStyled = styled.div`
   ${(props) => props.isHoverable && hoverableStyle}
   border-radius: ${theme.borderRadius.extraRound};
+  height: 330px;
   .card {
     background: ${theme.colors.white};
     box-sizing: border-box;
