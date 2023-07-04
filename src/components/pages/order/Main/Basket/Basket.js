1409f029129cf14ca3c140fa2243c063345a5af9
Tshoms <tshomsjordy@gmail.com>
2023-07-04 02:47:16 +0200

    feature/F10: Basket.js > add Footer.js w/ style .

--- src/components/pages/order/Main/Basket/Basket.js
+++ src/components/pages/order/Main/Basket/Basket.js
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import { theme } from "../../../../../theme";
 import { formatPrice } from "../../../../../utils/maths";
-import Header from "../../../../reusable-ui/Header";
 import Footer from "./Footer";
 import Total from "./Total";
 
@@ -10,11 +9,8 @@ function Basket() {
   return (
     <BasketStyled>
       <Total AmountToPay={formatPrice(0)} />
-
       <div className="body">body</div>
-      <Header>
-        <Footer />
-      </Header>
+      <Footer />
     </BasketStyled>
   );
 }
--- src/components/pages/order/Main/Basket/Footer.js
+++ src/components/pages/order/Main/Basket/Footer.js
@@ -1,31 +1,30 @@
 import React from "react";
 import styled from "styled-components";
 import { theme } from "../../../../../theme";
+import Header from "../../../../reusable-ui/Header";
 
 function Footer() {
   return (
-    <FooterStyled>
-      <span>Codé avec coeur et React!</span>
-    </FooterStyled>
+    <Header>
+      <FooterStyled>
+        <span>Codé avec ❤️ et React.JS</span>
+      </FooterStyled>
+    </Header>
   );
 }
 
 const FooterStyled = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   span {
-    color: white;
+    font-size: ${theme.fonts.size.P2};
+    font-family: ${theme.fonts.family.stylish};
+    font-weight: ${theme.fonts.weights.bold};
+    color: ${theme.colors.white};
   }
-
-  /* height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    span {
-        font-size: ${theme.fonts.size.P2};
-        font-family: ${theme.fonts.families.stylish};
-        font-weight: ${theme.fonts.weights.bold};
-        color: ${theme.colors.white};
-    } */
 `;
 
 export default Footer;
