f60ddec1091bb323effa194b9a825d96fe21fea0
Tshoms <tshomsjordy@gmail.com>
2023-07-04 20:41:50 +0200

    feature/F11: Basket.js > create BasketProduct.js and EmptyBsaket.js w/ conditional rendering algo of array basket.

--- src/components/pages/order/Main/Basket/Basket.js
+++ src/components/pages/order/Main/Basket/Basket.js
@@ -2,16 +2,23 @@ import React, { useContext } from "react";
 import styled from "styled-components";
 import OrderContext from "../../../../../context/OrderContext";
 import { formatPrice } from "../../../../../utils/maths";
+import BasketProduct from "./BasketProduct";
 import EmptyBasket from "./EmptyBasket";
 import Footer from "./Footer";
 import Total from "./Total";
 
 function Basket() {
   const { basket } = useContext(OrderContext);
+
+  const isBasketEmpty = basket.length === 0;
   return (
     <BasketStyled>
       <Total amountToPay={formatPrice(0)} />
-      <EmptyBasket basketProduct={basket} />
+      {isBasketEmpty ? (
+        <EmptyBasket basketProduct={basket} />
+      ) : (
+        <BasketProduct />
+      )}
       <Footer />
     </BasketStyled>
   );
---
+++ src/components/pages/order/Main/Basket/BasketProduct.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function BasketProduct() {
+  return <BasketProductStyled>BasketProduct</BasketProductStyled>;
+}
+
+const BasketProductStyled = styled.div``;
+
+export default BasketProduct;
