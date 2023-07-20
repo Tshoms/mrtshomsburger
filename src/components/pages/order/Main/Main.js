8970d4bfd98c6a5ebb97290803f98a8fc8cf51f8
Tshoms <tshomsjordy@gmail.com>
2023-07-20 10:29:06 +0200

    feature/F12: Main.js > refacto ...

--- src/components/pages/order/Main/Main.js
+++ src/components/pages/order/Main/Main.js
@@ -1,43 +1,27 @@
 import React from "react";
-import { useContext } from "react";
 import styled from "styled-components";
-import OrderContext from "../../../../context/OrderContext";
 import { theme } from "../../../../theme";
-import Admin from "./Admin/Admin";
 import Basket from "./Basket/Basket";
-import Menu from "./Menu/Menu";
+import MainRightSide from "./Menu/MainRightSide";
 
 export default function Main() {
-  // state -------
-  const { isModeAdmin } = useContext(OrderContext);
-
   return (
     <MainStyled>
       <Basket />
-      <div className="menu-and-admin">
-        <Menu />
-        {isModeAdmin && <Admin />}
-      </div>
+      <MainRightSide />
     </MainStyled>
   );
 }
 
 const MainStyled = styled.div`
-  background-color: ${theme.colors.background_white};
-  flex: 1;
+  background: ${theme.colors.background_white};
+  height: calc(95vh - 10vh);
 
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
-  /*css grid */
   display: grid;
   grid-template-columns: 25% 1fr; /* comportement en rows */
-  overflow-y: scroll; /*important pour le scroll */
-
-  .menu-and-admin {
-    position: relative;
-    overflow-y: hidden;
-    display: grid;
-  }
+  overflow: hidden;
 `;
---
+++ src/components/pages/order/Main/Menu/MainRightSide.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import styled from "styled-components";
+import OrderContext from "../../../../../context/OrderContext";
+import { theme } from "../../../../../theme";
+import Admin from "../Admin/Admin";
+import Menu from "./Menu";
+
+function MainRightSide() {
+  // state ---------
+  const { isModeAdmin } = useContext(OrderContext);
+
+  return (
+    <MainRightSideStyled>
+      <Menu />
+      {isModeAdmin && <Admin />}
+    </MainRightSideStyled>
+  );
+}
+
+const MainRightSideStyled = styled.div`
+  position: relative;
+  overflow-y: hidden;
+  display: grid;
+  border-bottom-right-radius: ${theme.borderRadius.extraRound};
+`;
+
+export default MainRightSide;
