6b125002d4b3438143b22efc728495975c6d7345
Tshoms <tshomsjordy@gmail.com>
2023-09-09 15:20:50 +0200

    main: refecto LoginForm.js

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -10,7 +10,7 @@ import Welcome from "./Welcome";
 
 function LoginForm() {
   // state -----------------
-  const [userName, setUserName] = useState("Jordy");
+  const [userName, setUserName] = useState("");
   const navigate = useNavigate();
 
   // comportement --------------
